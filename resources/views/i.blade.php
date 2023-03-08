cecdd981a41e183206085f239b2bc141300cb17c
Step1017 <s.aveta@yahoo.com>
2023-03-08 21:09:24 +0100

    I

--- resources/views/i.blade.php
+++ resources/views/i.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <title>Laravel Primi Passi</title>
+        <title>Laravel Primi Passi - I</title>
     </head>
     <body>
         <header class="fixed-top">
@@ -20,27 +20,27 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">
+                                <a class="nav-link" href="/">
                                     Home
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/i">
+                                <a class="nav-link active" aria-current="page" href="i.blade.php">
                                     I
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/am">
+                                <a class="nav-link" href="am.blade.php">
                                     AM
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/iron">
+                                <a class="nav-link" href="iron.blade.php">
                                     IRON
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/man">
+                                <a class="nav-link" href="man.blade.php">
                                     MAN
                                 </a>
                             </li>
@@ -52,19 +52,19 @@
 
             <main class="vh-100">
                 <div class="container h-100">
-                    <div class="row h-100 align-items-center">
-                        <div class="col">
-                            <h1 class="display-1 text-center fw-bold text-primary">
-                                Hello World with Laravel!
+                    <div class="row h-50">
+                        <div class="col d-flex justify-content-center align-items-center">
+                            <h1 class="display-1 fw-bold text-primary">
+                                I
                             </h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col d-flex justify-content-center align-items-center">
+                            <img class="d-block w-50"src="https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/10/5e60f12f-f461-451f-bf0e-e43c785cb1d7-696x364.jpg?fit=700%2C20000&quality=95&ssl=1" alt="i">
                         </div>
                     </div>
                 </div>
             </main>
     </body>
 </html>
-
-<!-- 1. Creo pagina home in views  -->
-<!-- <h1>
-    Hello World with Laravel!
-</h1> -->
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/i', function () {
+    return view('i');
+});
